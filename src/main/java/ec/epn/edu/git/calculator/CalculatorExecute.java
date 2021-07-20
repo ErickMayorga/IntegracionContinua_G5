e436eae6243cf382b5ae6f1231d3e5c4b4332a26
ErickMayorga <49161419+ErickMayorga@users.noreply.github.com>
2021-07-19 23:57:30 -0500

    Fixing calculator execute class

---
+++ src/main/java/ec/epn/edu/git/calculator/CalculatorExecute.java
@@ -0,0 +1,12 @@
+package ec.epn.edu.git.calculator;
+
+public class CalculatorExecute {
+    public static void main(String[] args){
+        System.out.println("== Calculator Execute ==");
+        Calculator c = new Calculator();
+        int addition = c.adition(4,7);
+        System.out.println("c.adition(4,7) = " + addition);
+        int substraction = c.substraction(7,1);
+        System.out.println("c.substraction(7,1) = " + substraction);
+    }
+}
